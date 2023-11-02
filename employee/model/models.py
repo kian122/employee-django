45b4058e3d83811b4b280e06f53971731c16668f
flawlessness-professional <mhrabykyan2@gmail.com>
2023-11-02 06:52:59 -0700

    adding the models

--- employee/model/models.py
+++ employee/model/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class employee(models.Model):
+    name = models.CharField( max_length=100)
+    last_name = models.CharField( max_length=100)
+    phone_number = models.IntegerField( max_length=20)
+    email = models.EmailField( max_length=255)
+    started_at = models.TimeField( auto_now=True)
