c2884bd2d03124cd50b3d973c7e1c5a53e11142f
flawlessness-professional <mhrabykyan2@gmail.com>
2023-11-02 08:13:57 -0700

    the log in part

--- employee/model/urls.py
+++ employee/model/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 # the import for views
 from . import views
 
+
+# log func import
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.frontpage , name="frontpage"),
-    path('log/', views.log),
+    path('login/' , auth_views.LoginView.as_view(template_name = "model/login.html") , name="login"),
     path('create/', views.create),
 ]
